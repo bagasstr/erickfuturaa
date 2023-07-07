f0c99873e09562e9a190cf50a68e5662e5a02959
bagasstr <bagassatrio2805@gmail.com>
2023-07-07 13:15:48 +0700

    first commit

--- src/Components/ProductDetail.jsx
+++ src/Components/ProductDetail.jsx
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { data } from "../Data/ListingTerbaru/DataTerbaru.json";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
 
 const ProductDetail = () => {
   const { title } = useParams();
@@ -9,11 +16,29 @@ const ProductDetail = () => {
 
   return (
     <div className="">
-      <div className="">
-        <div className="">
-          <img src={myData.thumnail} alt="" />
-          <h1 className="">{myData.title}</h1>
-        </div>
+      <div className="pt-28">
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          {myData.image.map((res, idx) => (
+            <SwiperSlide>
+              <PhotoProvider>
+                {myData.image.map((res, idx) => (
+                  <PhotoView key={idx} src={res}>
+                    {idx < 1 ? (
+                      <img src={res} alt="" className="desktop:hidden" />
+                    ) : undefined}
+                    {/* <img src={res} alt="" /> */}
+                  </PhotoView>
+                ))}
+              </PhotoProvider>
+              <img src={res} alt="" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
