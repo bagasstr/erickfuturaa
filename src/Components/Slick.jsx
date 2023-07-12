3457b779f5030d2aa3e529aef7636e49586dfe01
bagasstr <bagassatrio2805@gmail.com>
2023-07-12 23:34:56 +0700

    first commit

--- src/Components/Slick.jsx
+++ src/Components/Slick.jsx
@@ -5,32 +5,30 @@ const Slick = ({ props }) => {
     <div className="pb-10">
       <div className="flex gap-x-3 overflow-auto py-5">
         <div className="bg-white shadow-lg text-center py-2 px-3 rounded-md">
-          <h1 className="font-semibold text-base text-black/70">L.Bangunan</h1>
-          <p className="text-medium text-black/60 text-base">{props.lb}</p>
+          <h1 className="font-semibold text-sm text-black/70">L.Bangunan</h1>
+          <p className="text-medium text-black/60 text-xs">{props.lb}</p>
         </div>
         <div className="bg-white shadow-lg text-center py-2 px-3 rounded-md">
-          <h1 className="font-semibold text-base text-black/70">
+          <h1 className="font-semibold text-sm text-black/70">
             L.Tanah/Keseluruhan
           </h1>
-          <p className="text-medium text-black/60 text-base">{props.lbb}</p>
+          <p className="text-medium text-black/60 text-xs">{props.lbb}</p>
         </div>
         <div className="bg-white shadow-lg text-center py-2 px-3 rounded-md">
-          <h1 className="font-semibold text-base text-black/70">K.Mandi</h1>
-          <p className="text-medium text-black/60 text-base">{props.km}</p>
+          <h1 className="font-semibold text-sm text-black/70">K.Mandi</h1>
+          <p className="text-medium text-black/60 text-xs">{props.km}</p>
         </div>
         <div className="bg-white shadow-lg text-center py-2 px-3 rounded-md">
-          <h1 className="font-semibold text-base text-black/70">K.Tidur</h1>
-          <p className="text-medium text-black/60 text-base">{props.kt}</p>
+          <h1 className="font-semibold text-sm text-black/70">K.Tidur</h1>
+          <p className="text-medium text-black/60 text-xs">{props.kt}</p>
         </div>
         <div className="bg-white shadow-lg text-center py-2 px-3 rounded-md">
-          <h1 className="font-semibold text-base text-black/70">Lantai</h1>
-          <p className="text-medium text-black/60 text-base">{props.lantai}</p>
+          <h1 className="font-semibold text-sm text-black/70">Lantai</h1>
+          <p className="text-medium text-black/60 text-xs">{props.lantai}</p>
         </div>
         <div className="bg-white shadow-lg text-center py-2 px-3 rounded-md">
-          <h1 className="font-semibold text-base text-black/70">Pemandangan</h1>
-          <p className="text-medium text-black/60 text-base">
-            {props.pemandang}
-          </p>
+          <h1 className="font-semibold text-sm text-black/70">Pemandangan</h1>
+          <p className="text-medium text-black/60 text-xs">{props.pemandang}</p>
         </div>
       </div>
     </div>
