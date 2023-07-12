688afc32aa22cdb74dc33294cc2383f8b9501410
bagasstr <bagassatrio2805@gmail.com>
2023-07-12 23:12:30 +0700

    first commit

--- src/Components/Slick.jsx
+++ src/Components/Slick.jsx
@@ -1,26 +1,7 @@
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 import React from "react";
 
-const Slick = ({ props }) => {
-  return (
-    <div>
-      <Carousel showArrows={false} showStatus={false} showThumbs={false}>
-        <div className="">
-          <h1 className="">L.Tanah</h1>
-          <p className="">{props.lb}</p>
-        </div>
-        <div className="">
-          <h1 className="">L.Tanah</h1>
-          <p className="">{props.lb}</p>
-        </div>
-        <div className="">
-          <h1 className="">L.Tanah</h1>
-          <p className="">{props.lb}</p>
-        </div>
-      </Carousel>
-    </div>
-  );
+const Slick = () => {
+  return <div>Slick</div>;
 };
 
 export default Slick;
