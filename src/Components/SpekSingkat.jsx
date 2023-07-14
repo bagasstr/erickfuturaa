093752f5ce14f0e0fd78903145e6c5c584a7a8e0
bagasstr <bagassatrio2805@gmail.com>
2023-07-14 19:05:06 +0700

    firt commit

--- src/Components/SpekSingkat.jsx
+++ src/Components/SpekSingkat.jsx
@@ -8,36 +8,36 @@ const SpekSingkat = ({ props }) => {
       <div className="flex gap-x-3 overflow-auto py-5">
         <div className="bg-white shadow-lg text-center py-2 px-3 rounded-md">
           <h1 className="font-semibold text-sm text-black/70">L.Bangunan</h1>
-          <p className="text-medium text-black/60 text-xs">{props.lb}</p>
+          <p className="text-medium text-black/60 text-sm">{props.lb}</p>
         </div>
 
         <div className="bg-white shadow-lg text-center py-2 px-3 rounded-md">
           <h1 className="font-semibold text-sm text-black/70">
             L.Tanah/Keseluruhan
           </h1>
-          <p className="text-medium text-black/60 text-xs">{props.lbb}</p>
+          <p className="text-medium text-black/60 text-sm">{props.lbb}</p>
         </div>
         <div className="bg-white shadow-lg text-center py-2 px-3 rounded-md">
           <h1 className="font-semibold text-sm text-black/70">K.Mandi</h1>
-          <p className="text-medium text-black/60 text-xs">{props.km}</p>
+          <p className="text-medium text-black/60 text-sm">{props.km}</p>
         </div>
         <div className="bg-white shadow-lg text-center py-2 px-3 rounded-md">
           <h1 className="font-semibold text-sm text-black/70">K.Tidur</h1>
-          <p className="text-medium text-black/60 text-xs">{props.kt}</p>
+          <p className="text-medium text-black/60 text-sm">{props.kt}</p>
         </div>
         <div className="bg-white shadow-lg text-center py-2 px-3 rounded-md">
           <h1 className="font-semibold text-sm text-black/70">Lantai</h1>
-          <p className="text-medium text-black/60 text-xs">{props.lantai}</p>
+          <p className="text-medium text-black/60 text-sm">{props.lantai}</p>
         </div>
         <div className="bg-white shadow-lg text-center py-2 px-3 rounded-md">
           <h1 className="font-semibold text-sm text-black/70">Pemandangan</h1>
-          <p className="text-medium text-black/60 text-xs">{props.pemandang}</p>
+          <p className="text-medium text-black/60 text-sm">{props.pemandang}</p>
         </div>
       </div>
       <div className="px-4 pt-4">
         <h1
           onClick={() => setSpekLengkap((prev) => !prev)}
-          className="text-black/70 text-sm font-semibold"
+          className="text-black/70 text-xs font-semibold"
         >
           Lihat Selengkapnya
         </h1>
