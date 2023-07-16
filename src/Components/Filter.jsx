abd7274c8b8f1266fa2c1a97836d563719ab7470
bagasstr <bagassatrio2805@gmail.com>
2023-07-17 02:12:59 +0700

    first commit

--- src/Components/Filter.jsx
+++ src/Components/Filter.jsx
@@ -1,77 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Filter = () => {
+  const [type, setType] = useState("");
+  const [typeProperty, setTypeProperty] = useState("");
+  const [area, setArea] = useState("");
+
+  const navigate = useNavigate();
+
+  const handlerFilter = () => {
+    navigate(`/all-listings?area=${area}`);
+  };
+
   return (
     <div
-      className="w-full bg-gradient-to-r from-primary to-bg shadow-md py-16"
+      className="w-full bg-gradient-to-r from-primary to-bg shadow-md pb-8 pt-14"
       id="home"
     >
       <div className="max-w-[1024px] mx-auto px-2">
-        <div className="space-y-5">
-          <div className="text-center">
-            <form className="space-y-5">
-              <select
-                id="property"
-                className="bg-gray-50 border border-gray-300 text-primary font-medium text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 shadow-md"
-              >
-                <option selected className="text-xs">
-                  Type
-                </option>
-                <option value="US" className="text-xs">
-                  Apartemen
-                </option>
-                <option value="CA" className="text-xs">
-                  House
-                </option>
-                <option value="FR" className="text-xs">
-                  Cavling
-                </option>
-                <option value="DE" className="text-xs">
-                  Ruko
-                </option>
-              </select>
-              <select
-                id="property"
-                className="bg-gray-50 border border-gray-300 text-primary font-medium text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 shadow-md"
-              >
-                <option selected className="text-xs">
-                  Type Property
-                </option>
-                <option value="US" className="text-xs">
-                  Primary
-                </option>
-                <option value="CA" className="text-xs">
-                  Secondary
-                </option>
-              </select>
-              <select
-                id="area"
-                className="bg-gray-50 border border-gray-300 text-primary font-medium text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mobile:text-lg shadow-md"
-              >
-                <option selected className="text-xs">
-                  Area
-                </option>
-                <option value="US" className="text-xs">
-                  United States
-                </option>
-                <option value="CA" className="text-xs">
-                  Canada
-                </option>
-                <option value="FR" className="text-xs">
-                  France
-                </option>
-                <option value="DE" className="text-xs">
-                  Germany
-                </option>
-              </select>
+        <div className="">
+          <h1 className="mb-3 font-medium text-lg text-white">
+            Cari Properti Dengan Mudah
+          </h1>
+          <div className="text-center space-y-5">
+            <select
+              id="area"
+              className="bg-gray-50 border border-gray-300 text-primary font-medium text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mobile:text-lg shadow-md"
+              value={area}
+              onChange={(e) => setArea(e.target.value)}
+            >
+              <option selected className="text-xs">
+                Pilih Area Property
+              </option>
+              <option value="Jakarta Barat" className="text-xs">
+                Jakarta Barat
+              </option>
+              <option value="Tangerang" className="text-xs">
+                Tangerang
+              </option>
+            </select>
 
-              <button
-                type="submit"
-                className="bg-white px-4 py-2 rounded-md mt-6 shadow-md text-lg font-semibold w-full text-primary"
-              >
-                Search
-              </button>
-            </form>
+            <button
+              onClick={handlerFilter}
+              className="bg-white px-4 py-2 rounded-md mt-6 shadow-md text-lg font-semibold w-full text-primary"
+            >
+              Search
+            </button>
           </div>
         </div>
       </div>
--- src/Pages/AllProduct.jsx
+++ src/Pages/AllProduct.jsx
@@ -1,7 +1,109 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import data from "../Data/ListinganPopular/DataPopular.json";
+import { Link, useLocation } from "react-router-dom";
+
+import { FaBath, FaBed, FaLocationDot } from "react-icons/fa6";
+import { GiResize } from "react-icons/gi";
+import { BsFillBuildingFill } from "react-icons/bs";
 
 const AllProduct = () => {
-  return <div className="py-20">AllProduct</div>;
+  const location = useLocation();
+  let filterOption;
+  filterOption = new URLSearchParams(location.search).get("area");
+
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  useEffect(() => {
+    const filtered = filterOption
+      ? data.filter((product) => product.area === filterOption)
+      : data;
+    setFilteredProducts(filtered);
+  }, [filterOption]);
+  return (
+    <div>
+      <div className="w-full">
+        <div className="containers px-2 py-16">
+          <div className="">
+            {/* Listingan Terbaru */}
+            <div className="ListinganTerbaru">
+              <div className="flex flex-col items-center">
+                {filteredProducts.map((res, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className="shadow-lg rounded-md w-fit my-7 pb-4"
+                    >
+                      <div className="relative">
+                        <img
+                          src={res.thumnail}
+                          alt="thumnail"
+                          className="rounded-md shadow-lg"
+                        />
+                        <h1 className="absolute text-white bg-primary px-3 py-1 rounded-md font-semibold text-lg top-0">
+                          {res.market.toUpperCase()}
+                        </h1>
+                      </div>
+                      <div className="p-4 rounded-md text-text">
+                        <h1 className="font-semibold text-text/60 mb-2">
+                          {res.type}
+                        </h1>
+                        <h1 className="text-2xl font-semibold">{res.title}</h1>
+                        <div className="flex items-center gap-x-2 mb-4 text-text/80">
+                          <FaLocationDot className="text-sm" />
+                          <h2 className="text-sm font-medium">{res.area}</h2>
+                        </div>
+                        <div className="mb-4">
+                          <h1 className="text-lg font-medium text-text/60">
+                            Harga mulai dari
+                          </h1>
+                          <h3 className="text-2xl font-medium">{res.harga}</h3>
+                        </div>
+                        <hr className="my-2 bg-text/20" />
+                        <div className="flex gap-x-4 justify-center">
+                          <div className="flex items-center gap-x-2">
+                            <FaBath className="text-xl" />
+                            <h1 className="text-lg font-medium">{res.km}</h1>
+                          </div>
+                          <div className="flex items-center gap-x-2">
+                            <FaBed className="text-xl" />
+                            <h1 className="text-lg font-medium">{res.kt}</h1>
+                          </div>
+                          <div className="flex items-center gap-x-2">
+                            <GiResize className="text-xl" />
+                            <h1 className="text-lg font-medium">{res.lb}</h1>
+                          </div>
+                          <div className="flex items-center gap-x-2">
+                            <BsFillBuildingFill className="text-xl" />
+                            <h1 className="text-lg font-medium">{res?.lbb}</h1>
+                          </div>
+                        </div>
+                        <hr className="my-2 bg-text/20" />
+                        <div className="mt-10 flex justify-around">
+                          <Link
+                            to={`/listings/detail/${res.title}`}
+                            preventScrollReset={false}
+                            className="border-2 border-text text-text px-4 py-3 uppercase rounded-md font-semibold shadow-md flex items-center"
+                          >
+                            Detail Unit
+                          </Link>
+
+                          <a
+                            href=""
+                            className="bg-text text-white px-4 py-3 uppercase rounded-md font-semibold flex items-center shadow-md"
+                          >
+                            WhatsApp
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AllProduct;
