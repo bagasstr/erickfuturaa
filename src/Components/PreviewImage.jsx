342c5b6db7478475fec631438e184c317d1ce2e9
bagasstr <bagassatrio2805@gmail.com>
2023-07-17 15:03:54 +0700

    first commit

--- src/Components/PreviewImage.jsx
+++ src/Components/PreviewImage.jsx
@@ -7,6 +7,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 
 import "swiper/css";
+import ButtonWa from "./ButtonWa";
 
 const PreviewImage = ({ props }) => {
   const { title } = useParams();
@@ -14,30 +15,37 @@ const PreviewImage = ({ props }) => {
   useEffect(() => {}, [myData]);
   return (
     <div className="fixed pt-[4.2rem] top-0 right-0 bg-black w-screen h-screen z-50">
-      <div className="z-50 absolute right-7 top-24">
+      <div className="flex items-center justify-between px-3 py-5">
+        <div className="">
+          <h1 className="text-white/90 text-sm font-medium">{myData.title}</h1>
+          <h1 className="text-white/90 text-sm font-medium">{myData.area}</h1>
+        </div>
         <AiOutlineCloseCircle
           onClick={() => props((prev) => !prev)}
           className="text-4xl rounded-full shadow-lg text-white "
         />
       </div>
-      <Swiper modules={[]} spaceBetween={30} slidesPerView={1}>
-        {myData.image.map((res, idx) => (
-          <div key={idx} className="">
-            <SwiperSlide>
-              <div className="">
+      <ButtonWa />
+      <div className="">
+        <Swiper modules={[]} spaceBetween={30} slidesPerView={1}>
+          {myData.image.map((res, idx) => (
+            <div key={idx} className="">
+              <SwiperSlide>
                 <div className="">
-                  <img
-                    src={res}
-                    alt=""
-                    onClick={() => setPreview((prev) => !prev)}
-                    className="w-full h-[400px] object-cover cursor-pointer"
-                  />
+                  <div className="">
+                    <img
+                      src={res}
+                      alt=""
+                      onClick={() => setPreview((prev) => !prev)}
+                      className="w-full h-[400px] object-cover cursor-pointer"
+                    />
+                  </div>
                 </div>
-              </div>
-            </SwiperSlide>
-          </div>
-        ))}
-      </Swiper>
+              </SwiperSlide>
+            </div>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
