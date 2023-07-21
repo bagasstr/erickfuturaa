e1c5b1c2a305b88b3a91ef4b48c100b3f6a32b71
bagasstr <bagassatrio2805@gmail.com>
2023-07-21 16:28:38 +0700

    first commit

--- src/Components/PreviewImage.jsx
+++ src/Components/PreviewImage.jsx
@@ -59,7 +59,7 @@ const PreviewImage = ({ props }) => {
               </div>
             ))}
           </Swiper>
-          <div className="absolute bottom-10 left-0 right-0 desktop:w-[500px] desktop:mx-auto">
+          <div className="absolute bottom-28 left-0 right-0 desktop:w-[500px] desktop:mx-auto">
             <ButtonWa />
           </div>
         </div>
