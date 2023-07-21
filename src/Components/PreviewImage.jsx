7c75b8042b1d22601f46e4542c3599b419ad71b9
bagasstr <bagassatrio2805@gmail.com>
2023-07-21 16:26:06 +0700

    first commit

--- src/Components/PreviewImage.jsx
+++ src/Components/PreviewImage.jsx
@@ -22,7 +22,7 @@ const PreviewImage = ({ props }) => {
       <Navbar hidden={"hidden"} />
       <div
         id="imagePreview"
-        className="fixed top-0 right-0 bg-black w-screen z-[99] image-preview"
+        className="fixed top-0 right-0 bg-black w-screen h-screen z-[99] image-preview"
       >
         <div className="flex items-center justify-around mobile:justify-between mobile:px-4 py-5">
           <div className="">
@@ -59,7 +59,7 @@ const PreviewImage = ({ props }) => {
               </div>
             ))}
           </Swiper>
-          <div className="desktop:w-[500px] desktop:mx-auto">
+          <div className="absolute bottom-4 left-0 right-0 desktop:w-[500px] desktop:mx-auto">
             <ButtonWa />
           </div>
         </div>
