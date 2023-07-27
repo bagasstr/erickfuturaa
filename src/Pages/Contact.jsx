e0bf4d83dcef5e3a541b8c675fde262e4b833ed9
bagasstr <bagassatrio2805@gmail.com>
2023-07-27 18:26:36 +0700

    modified page contact

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -13,26 +13,36 @@ const Contact = () => {
             className="object-cover brightness-75 w-full h-full"
           />
         </div>
-        <div className="desktop:bg-white mobile:bg-black/80 mobile:mx-4 px-10 py-16 mobile:px-6 absolute rounded-lg shadow-md desktop:right-[15rem]">
-          <div className="desktop:ml-16">
-            <h1 className="text-black/90 mobile:text-white/95 font-bold text-3xl mb-4">
+        <div className="desktop:bg-white absolute mobile:bg-black/80 desktop:px-10 desktop:py-16 mobile:py-5 mobile:px-3 rounded-lg shadow-md desktop:right-[15rem] mobile:w-[300px] mobile:right-0 mobile:left-0 mobile:mx-auto">
+          <div className="desktop:ml-16 mobile:w-full">
+            <h1 className="text-black/90 mobile:text-2xl mobile:text-white/95 font-bold text-3xl mb-4">
               Hubungi Saya
             </h1>
-            <h2 className="text-black/80 mobile:text-white/95 text-base desktop:w-[30rem] mb-5 font-medium">
+            <h2 className="text-black/80 mobile:text-white/95 text-base desktop:w-[30rem] mb-5 font-medium mobile:text-sm">
               Jika ada pertanyaan atau konsultasi seputar property silahkan
               Hubungi saya di bawah.
             </h2>
             <div className="flex items-center gap-x-2 mb-2 mobile:text-white/95 text-black/75">
-              <BsFillTelephoneFill className="text-lg" />
-              <h1 className=" font-semibold text-base">+62 877-89070-757</h1>
+              <div>
+                <BsFillTelephoneFill className="text-lg" />
+              </div>
+              <h1 className="font-semibold text-base mobile:text-sm">
+                +62 877-89070-757
+              </h1>
             </div>
             <div className="flex items-center gap-x-2 mb-2 mobile:text-white/95 text-black/75">
-              <BsFillBuildingFill className="text-lg" />
-              <h1 className=" font-semibold text-base">+62 822-3333-5227</h1>
+              <div>
+                <BsFillBuildingFill className="text-lg" />
+              </div>
+              <h1 className="font-semibold text-base mobile:text-sm">
+                +62 822-3333-5227
+              </h1>
             </div>
             <div className="flex items-center gap-x-2 mobile:text-white/95 text-black/75">
-              <AiFillMail className="text-lg" />
-              <h1 className=" font-semibold text-base">
+              <div>
+                <AiFillMail className="text-lg" />
+              </div>
+              <h1 className="font-semibold text-base mobile:text-sm">
                 erickfuturaproperty@gmail.com
               </h1>
             </div>
